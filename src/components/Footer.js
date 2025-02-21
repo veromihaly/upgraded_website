e973dc4897f224052543109d07978f9fd7ed5c26
Verő_Mihály <mrdttyb480222@mailbox.unideb.hu>
2025-02-21 19:22:16 +0100

    Deleted unecessary files and imported tailwind.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="bg-gray-900 text-white p-4 text-center">
-            <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Verő Mihály. All Rights Reserved.</p>
         </footer>
     );
 }
