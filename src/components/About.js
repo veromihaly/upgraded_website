617fc14fdef65f873b928a8fe2c7ccfa2b8a44dc
Verő_Mihály <mrdttyb480222@mailbox.unideb.hu>
2025-06-20 15:11:32 +0200

    Upgraded the website about me page and added a download resume button

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,17 @@ function About(){
         <section id="about" className="p-6">
             <h2 className="text-2xl font-bold text-red-500">About me</h2>
             <p className="text-white">
-            I'm a student at the University of Debrecen, Faculty of Informatics. I've completed all my classes and only the final exam remains. With a passion for technology and problem-solving,
-            I’ve worked hard to build a solid foundation in programming and software development. I hold certifications in Python (IT Specialist), HTML and CSS (IT Specialist) and
-            Unity (Certified User: Programmer), and I have strong skills in C# and other programming languages. I thrive in both team settings and independent work, and I’m always eager to learn 
-            and take on new challenges. While I’m still early in my professional journey, I’m confident that my dedication and quick learning ability will allow me to make an impact in the tech 
-            industry. In my free time, I enjoy staying active, watching English-language films and series, and diving into video games. Feel free to explore my projects on my <a href="https://github.com/veromihaly" target="_blank" rel="noopener noreferrer" className="text-red-500 hover:text-[#ff6347] hover:underline visited:text-[#d32f2f]">GitHub</a> to see more of what I’m working on!
+                I'm a recent graduate from the University of Debrecen, Faculty of Informatics. My final project was a 3D survival RPG game built in Unity, where I explored AI, animations, sound design, and more. 
+                I hold certifications in Python (IT Specialist), HTML and CSS (IT Specialist), and Unity (Certified User: Programmer). I have strong skills in C#, Unity, and web technologies, and I love taking on challenges that stretch my problem-solving and creativity.
+                I thrive in both team environments and independent projects, and I’m currently open to opportunities in software development, especially in game dev, frontend, or full-stack roles.
+                In my free time, I enjoy gaming, drawing, and watching English-language series and films. 
             </p>
+            <p className="text-white">
+                Feel free to explore my projects on <a href="https://github.com/veromihaly" target="_blank" className="text-red-500 hover:text-[#ff6347] hover:underline visited:text-[#d32f2f]">GitHub</a> or download my resume right here:
+            </p>
+            <a href="CV.pdf" download="Mihaly_Vero_Resume.pdf" target="_blank" class="inline-block mt-4 px-6 py-3 bg-[#F03C46] text-white font-semibold rounded-lg shadow hover:bg-red-700 transition">
+                Download My Resume
+            </a>
         </section>
     );
 }
